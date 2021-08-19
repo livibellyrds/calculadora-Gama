94d02fe2771f615ee4374e63e9113d296597a737
Danielle Rodrigues <72668799+livibellyrds@users.noreply.github.com>
2021-08-19 19:34:54 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,16 @@
-/*function soma(valor1,valor2){
-  return valor1 + valor2
-}
-function subtracao(valor1,valor2){
-  return valor1 - valor2
-}
-function multiplicacao(valor1,valor2){
-  return valor1 * valor2
-}
-function divisao(valor1,valor2){
-  return valor1 / valor2
-}
-
-
-console.log(soma(10,8))
-console.log(subtracao(10,8))
-console.log(multiplicacao(2,1))
-console.log(divisao(25,5))*/
-   
 
 const soma = (valor1,valor2) =>{
   return valor1 + valor2
-}
+};
 const subtracao = (valor1,valor2) =>{
   return valor1 - valor2
-}
+};
 const multiplicacao = (valor1,valor2) =>{
   return valor1 * valor2
-}
+};
 const divisao = (valor1,valor2) =>{
   return valor1 / valor2
-}
+};
 
 console.log(soma(10,8))
 console.log(subtracao(10,8))
