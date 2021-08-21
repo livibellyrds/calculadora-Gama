6a9aecaa92e8cbee754ddb1943abf65c90cc9ea0
Danielle Rodrigues <72668799+livibellyrds@users.noreply.github.com>
2021-08-21 10:14:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,4 @@ const divisao = (valor1,valor2) =>{
   return valor1 / valor2
 };
 
-console.log(soma(10,8))
-console.log(subtracao(10,8))
-console.log(multiplicacao(2,1))
-console.log(divisao(25,5))
+
